3ad0cd52759189f787c6cc8ba4e6ecf5fd0f08aa
Mikulas Vesely <mikface@gmail.com>
2022-10-31 19:19:46 +0100

    fix hadoopVsLinuxCommands.sh (remove wget, use repo files)

--- shell/hadoopVsLinuxCommands.sh
+++ shell/hadoopVsLinuxCommands.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/KiksZero/CloudComputingAssignment2/main/WordCount.java
-hadoop com.sun.tools.javac.Main WordCount.java
-jar cf wc.jar WordCount*.class
-wget https://raw.githubusercontent.com/KiksZero/CloudComputingAssignment2/main/pg4300.txt
-hdfs dfs -mkdir input
-hdfs dfs -copyFromLocal pg4300.txt input
+# setting file name for time comparison result
+FILE_NAME="hadoopVsLinux.time"
 
-time cat pg4300.txt | tr ' ' '\n' | sort | uniq -c > /dev/null
-time hadoop jar wc.jar WordCount input output
+# compile java code of java solution
+cd ./java
+hadoop com.sun.tools.javac.Main WordCount.java
+jar cf wordcount.jar WordCount*.class
+cd ..
+
+# hdfs preparation (including input file)
+hdfs dfs -rm -r wordCountOutput
+hdfs dfs -rm -r wordCountInput
+hdfs dfs -mkdir wordCountInput
+hdfs dfs -copyFromLocal data/pg4300.txt wordCountInput
+
+# run linux and hadoop command for time comparison
+echo 'Running Linux Command...'
+echo 'Linux time:' >$FILE_NAME
+{ time cat data/pg4300.txt | tr ' ' '\n' | sort | uniq -c >/dev/null; } 2>&1 | tail -n3 >>$FILE_NAME
+echo >>$FILE_NAME
+echo 'Running Hadoop...'
+echo 'Hadoop time:' >>$FILE_NAME
+{ time `hadoop jar java/wordcount.jar WordCount wordCountInput wordCountOutput`; } 2>&1 | tail -n3 >>$FILE_NAME
+
+# cleanup class and jar files
+rm java/WordCount*.class
+rm java/wordcount.jar
