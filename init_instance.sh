f6c96ddfa12f1ebc779478b33cd53fc8f6b1af9a
Marc Garcia <marcgarciamassaneda@gmail.com>
2022-10-31 00:23:28 -0400

    replace HADOOP_PREFIX with HADOOP_HOME (deprecation fix)

--- init_instance.sh
+++ init_instance.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install openjdk-8-jdk-headless -y
@@ -8,9 +9,8 @@ tar -xf hadoop-3.3.4.tar.gz -C /usr/local/
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /home/ubuntu/.profile
 source /home/ubuntu/.profile
 echo "export PATH=$JAVA_HOME/bin:$PATH" >> /home/ubuntu/.profile
-echo "export HADOOP_PREFIX=/usr/local/hadoop-3.3.4" >> /home/ubuntu/.profile
+echo "export HADOOP_HOME=/usr/local/hadoop-3.3.4" >> /home/ubuntu/.profile
 source /home/ubuntu/.profile
-echo "export PATH=$HADOOP_PREFIX/bin:$PATH" >> /home/ubuntu/.profile
-echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\nexport HADOOP_PREFIX=/usr/local/hadoop-3.3.4" >> /etc/hadoop/hadoop-env.sh
-
-
+echo "export PATH=$HADOOP_HOME/bin:$PATH" >> /home/ubuntu/.profile
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\nexport HADOOP_HOME=/usr/local/hadoop-3.3.4" >> /etc/hadoop/hadoop-env.sh
+echo "export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar" >> /home/ubuntu/.profile
