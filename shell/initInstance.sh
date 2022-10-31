830f296756d7def85731c15bca0d42231133b45a
Mikulas Vesely <mikface@gmail.com>
2022-10-31 20:07:21 +0100

    rename initInstance script to match camelCase, clone repo into home folder after instance initialization

--- shell/initInstance.sh
+++ shell/initInstance.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+# update instance packages and install java 8 jdk
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install openjdk-8-jdk-headless -y
 su ubuntu
+
+# download and install hadoop
 sudo curl -o hadoop-3.3.4.tar.gz "https://dlcdn.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz"
 tar -xf hadoop-3.3.4.tar.gz -C /usr/local/
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /home/ubuntu/.profile
@@ -14,5 +17,11 @@ source /home/ubuntu/.profile
 echo "export PATH=$HADOOP_HOME/bin:$PATH" >> /home/ubuntu/.profile
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\nexport HADOOP_HOME=/usr/local/hadoop-3.3.4" >> /etc/hadoop/hadoop-env.sh
 echo "export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar" >> /home/ubuntu/.profile
+
+# install spark
 sudo apt install python3-pip -y
 pip install pyspark
+
+# clone our repo with all scripts, java files and data inputs
+cd ~
+git clone https://github.com/KiksZero/CloudComputingAssignment2.git
