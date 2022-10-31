069f425e68762c15ae2ea221b9b1157457a9d78c
Mikulas Vesely <mikface@gmail.com>
2022-10-31 07:02:51 +0100

    fix runMapReduce.sh (after directory restruct.)

--- shell/runMapReduce.sh
+++ shell/runMapReduce.sh
@@ -12,24 +12,26 @@ rm selectedIdsResult 2> /dev/null
 hdfs dfs -rm -r friendsOutput
 hdfs dfs -rm -r friendsInput
 hdfs dfs -mkdir friendsInput
-hdfs dfs -copyFromLocal ../data/FriendsNetworkInput.txt friendsInput
+hdfs dfs -copyFromLocal data/FriendsNetworkInput.txt friendsInput
 
 # compile java code of our solution
+cd ./java
 hadoop com.sun.tools.javac.Main FriendsMapReduce.java
 jar cf friends.jar FriendsMapReduce*.class
 
 # run hadoop
-hadoop jar friends.jar FriendsMapReduce friendsInput friendsOutput
+cd ..
+hadoop jar java/friends.jar FriendsMapReduce friendsInput friendsOutput
 
 # cleanup class and jar files
-rm FriendsMapReduce*.class
-rm friends.jar
+rm java/FriendsMapReduce*.class
+rm java/friends.jar
 
 # save results for specified ids into file
 touch selectedIdsResult
 for NUMBER in "$@"
 do
-  grep "^$NUMBER\s.*\$" ./friendsOutput/part-r-00000 >> selectedIdsResult
+  grep "^$NUMBER\s.*\$" friendsOutput/part-r-00000 >> selectedIdsResult
 done
 
-echo 'DONE! Run "cat ./selectedIdsResult" for top 10 friend suggestions for selected user IDs'
+echo 'DONE! See "selectedIdsResult" file in project root for top 10 friend suggestions for selected user IDs'
